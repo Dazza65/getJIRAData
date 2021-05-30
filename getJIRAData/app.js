af34447a7277af7659d941973a776512dc9891c7
dazza65x <dazza@me.com>
2021-05-30 11:08:31 +0800

    Initial commit

---
+++ getJIRAData/app.js
@@ -0,0 +1,69 @@
+const axios = require('axios');
+const { S3Client, PutObjectCommand }  = require('@aws-sdk/client-s3');
+const { SSMClient, GetParameterCommand } = require("@aws-sdk/client-ssm");
+
+con
+const jql = encodeURIComponent("project = SSP AND type = Story AND status = Done"); 
+const url = `https://koondart.atlassian.net/rest/api/3/search?jql=${jql}&maxResults=2&fields=id,key,summary,created,resolutiondate`;
+
+const getData = async () => {
+
+    const ssmClient = new SSMClient({region: 'ap-southeast-2'});
+    const userName = await ssmClient.send(new GetParameterCommand({Name: '/getJIRAData/username'}));
+    const token = await ssmClient.send(new GetParameterCommand({Name: '/getJIRAData/token'}));
+
+    const auth_token = Buffer.from(`${userName.Parameter.Value}:${token.Parameter.Value}`, 'utf8').toString('base64')
+
+    axios.defaults.headers.Authorization = `Basic ${auth_token}`;
+
+    const res = await axios.get(url, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const issues = res.data.issues.map( (issue) => {
+        return( {
+            id: issue.id,
+            key: issue.key,
+            summary: issue.fields.summary,
+            dateCreated: issue.fields.created,
+            dateResolved: issue.fields.resolutiondate
+        });
+    });
+
+    console.log(JSON.stringify(issues));
+
+    return issues;
+};
+
+const putObject = async (issues) => {
+    const client = new S3Client({region: 'ap-southeast-2'});
+
+    const currentDate = new Date().toISOString().substr(0, 19);
+    const fileName = `JIRAdata.${currentDate}`;
+    const params = {
+        Bucket: 'sam-jiradata-dh',
+        Key: fileName,
+        Body: JSON.stringify(issues)
+    };
+
+    try {
+        const data = await client.send(new PutObjectCommand(params));
+        console.log(`Uploaded to ${fileName}`);
+    }
+    catch(err) {
+        throw("S3Client ERROR: " + err);
+    }
+};
+
+exports.lambdaHandler = async (event, context) => {
+    try {
+        const issues = await getData();
+
+        await putObject(issues);
+   } catch (e) {
+       console.log(e);
+       throw (e);
+   } 
+};
